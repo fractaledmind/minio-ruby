cccaaabd13da1de1bf930a89ea2f540dc3724e88
Stephen Margheim <stephen.margheim@gmail.com>
2024-04-29 21:47:21 +0200

    Improve the native platforms list

--- lib/minio/upstream.rb
+++ lib/minio/upstream.rb
@@ -6,9 +6,11 @@ module MinIO
     NATIVE_PLATFORMS = {
       "arm64-darwin" => "darwin-arm64/minio.#{VERSION}",
       "arm64-linux" => "linux-arm64/minio.#{VERSION}",
+      "aarch64-linux" => "linux-arm64/minio.#{VERSION}",
       "x86_64-darwin" => "darwin-amd64/minio.#{VERSION}",
       "x86_64-linux" => "linux-amd64/minio.#{VERSION}",
-      "x86_64-windows" => "windows-amd64/minio.#{VERSION}"
+      "x64-mingw32" => "windows-amd64/minio.#{VERSION}",
+      "x64-mingw-ucrt" => "windows-amd64/minio.#{VERSION}"
     }
   end
 end
